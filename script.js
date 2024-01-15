8ca30554fd027107c9887edad4eeb0422934bd42
majazimnoch <majamzimnoch@gmail.com>
2024-01-15 11:09:36 +0100

    basic web is done

--- script.js
+++ script.js
@@ -1,59 +1,63 @@
 const art = document.getElementById("art");
+let currentPage = 1;
+const limit = 100;
+
 const artworkDetails = async (artworkId) => {
     const response = await fetch(`https://api.artic.edu/api/v1/artworks/${artworkId}`);
     return await response.json();
 }
 
-fetch("https://api.artic.edu/api/v1/artworks")
-  .then((response) => response.json())
-  .then((json) => {
-    const artworks = json.data;
+const getRandomArtwork = () => {
+    const apiUrl = 'https://api.artic.edu/api/v1/artworks';
 
-    // Total numbers of artworks
-    art.innerHTML = `
-        <h1>There are ${json.pagination.total} artworks in this database.</h1>
-        <a href="http://127.0.0.1:5500/index.html" id="reload" class="button">Click here to get a new artwork</a>
-        `;
+    fetch(`${apiUrl}?page=${currentPage}&limit=${limit}`)
+        .then((response) => response.json())
+        .then((json) => {
+            const artworks = json.data;
 
-    // Here we get a random index to later on select a random artwork
-    const randomIndex = Math.floor(Math.random() * artworks.length);
-    const randomArtwork = artworks[randomIndex];
+            // Get a random index to select a random artwork
+            const randomIndex = Math.floor(Math.random() * artworks.length);
+            const randomArtwork = artworks[randomIndex];
 
-     // Display information about the random artwork
-     if (randomArtwork) {
-        const title = randomArtwork.title;
-        const artistDisplay = randomArtwork.artist_display;
-        const imageId = randomArtwork.image_id;
-        
-        // Constructing the Image API URL
-        const iiifUrl = "https://www.artic.edu/iiif/2";
-        const imageUrl = `${iiifUrl}/${imageId}/full/400,/0/default.jpg`;
+            // Display information about the random artwork
+            if (randomArtwork) {
+                const title = randomArtwork.title;
+                const artistDisplay = randomArtwork.artist_display;
+                const imageId = randomArtwork.image_id;
 
-        artworkDetails(randomArtwork.id)
-            .then((details) => {
-                const description = details.data.description || "We have no description for this artwork, yet!"
+                // Constructing the Image API URL
+                const iiifUrl = "https://www.artic.edu/iiif/2";
+                const imageUrl = `${iiifUrl}/${imageId}/full/400,/0/default.jpg`;
 
-            // Info about a random artwork
-            art.innerHTML += `
-                <div class="artwork-container">
-                    <h2>${title}</h2>
-                    <h3>${artistDisplay}<h3>
-                    <img src="${imageUrl}" alt="${title}" class="artwork-image">
-                    <p>${description}</p>
-                </div>
-            `;
-            
-            })
-            .catch((error) => {
-                console.error('Error fetching artwork details:', error)
-            });
+                artworkDetails(randomArtwork.id)
+                    .then((details) => {
+                        const description = details.data.description || "We have no description for this artwork, yet!"
 
-    } else {
-      art.innerHTML += `<p>No artworks available.</p>`;
-    }
-  })
-  .catch((error) => {
-    console.error("Error fetching data");
-    art.innerHTML = `<p>Error fetching data.</p>`;
-  });
+                        // Info about a random artwork
+                        art.innerHTML = `
+                            <button id="reload" class="button" onclick="getRandomArtwork()">Click here to get a new artwork</button>
+                            <div class="artwork-container">
+                                <h2>${title}</h2>
+                                <h3>${artistDisplay}</h3>
+                                <img src="${imageUrl}" alt="${title}" class="artwork-image">
+                                <p>${description}</p>
+                            </div>
+                        `;
 
+                    })
+                    .catch((error) => {
+                        console.error('Error fetching artwork details:', error)
+                    });
+
+            } else {
+                art.innerHTML += `<p>No artworks available.</p>`;
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching data");
+            art.innerHTML = `<p>Error fetching data.</p>`;
+        });
+}
+
+// Initial display of a random artwork
+getRandomArtwork();
